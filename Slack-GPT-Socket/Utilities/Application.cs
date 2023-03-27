6b5cd4626a7a1a667aeda23601d6bd96d83f3963
Tomasz Juszczak <tomasz.juszczak@prographers.com>
2023-03-27 19:30:26 +0200

    Added comments to the ApiSettings

--- Slack-GPT-Socket/Utilities/Application.cs
+++ Slack-GPT-Socket/Utilities/Application.cs
@@ -2,20 +2,29 @@
 
 namespace Slack_GPT_Socket;
 
+/// <summary>
+///     Information about the application.
+/// </summary>
 public class Application
 {
     static Application()
     {
         // Read the version from version.txt
         var versionRaw = File.ReadAllText("version.txt");
-        if (!Version.TryParse(versionRaw.Trim('"', 'v').Trim(), out var versionParsed))
+        if (!Version.TryParse(versionRaw.Trim('"', 'v', '\n', '\r').Trim(), out var versionParsed))
         {
             Console.WriteLine($"Failed to parse version.txt\n\"{versionRaw}\"");
         }
         Version = versionParsed ?? new Version(1, 0, 0, 0);
     }
 
+    /// <summary>
+    ///     The version of the application.
+    /// </summary>
     public static Version Version { get; }
     
+    /// <summary>
+    ///     The version of the application as a string.
+    /// </summary>
     public static string VersionString => $"v{Version.Major}.{Version.Minor}.{Version.Build}";
 }
